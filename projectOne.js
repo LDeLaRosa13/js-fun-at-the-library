adc7f41453559d6b41ddc2f401f98500dca569fe
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-03-23 10:47:15 -0400

    Update practice project

---
+++ projectOne.js
@@ -0,0 +1,38 @@
+var title = 'Here We Go Again'
+var pages = 392
+
+pages = 392/4;
+console.log(pages);
+
+
+var movie 
+console.log(movie);
+
+var sequel = (title + ' Part Two')
+console.log(sequel);
+
+var isFavorite = true
+console.log(isFavorite);
+
+
+
+var isShortStory = pages < 25;
+var pages = 34;
+
+console.log(isShortStory);
+
+
+
+var pages = 198;
+if (pages < 1000) {
+  console.log('I could read that in a day!') 
+} else {
+  console.log('Okay, maybe I could read that in a day and a half')
+}
+
+var isFavorite = false;
+if (isFavorite === true) {
+console.log(`My favorite book is ${title}!`)
+} else {
+console.log(`I recently read the ${title} book`)
+}
