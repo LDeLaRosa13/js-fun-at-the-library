3009bc830f1a93b344866863c26eef1dd540738c
Lauren DeLaRosa <lstoneman13@gmail.com>
2023-03-26 15:21:34 -0400

    update library to checkOut

--- src/library.js
+++ src/library.js
@@ -1,8 +1,51 @@
 
+function createLibrary (libraryName) {
+  var denverLibrary = {};
+  denverLibrary.name = libraryName;
+  denverLibrary.shelves = {
+    fantasy: [],
+    fiction: [],
+    nonFiction: [],
+
+  };
+  return denverLibrary;
+}
+
+function addBook(denverLibrary, book) {
+  if (book.genre === 'fantasy') {
+    denverLibrary.shelves.fantasy.push(book);
+  } else if (book.genre === 'fiction') {
+    denverLibrary.shelves.fiction.push(book);
+  } else if (book.genre === 'nonFiction') {
+    denverLibrary.shelves.nonFiction.push(book);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
